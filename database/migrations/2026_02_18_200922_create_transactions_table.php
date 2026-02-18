f0588df115cc0f84f2d8f14e3a24cea480ad7e13
Koumolou <learningsin2024@gmail.com>
2026-02-18 21:25:50 +0100

    Transaction database

---
+++ database/migrations/2026_02_18_200922_create_transactions_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+                 $table->id();
+
+        $table->foreignId('sender_id')
+              ->nullable()
+              ->constrained('users')
+              ->nullOnDelete();
+
+        $table->foreignId('receiver_id')
+              ->nullable()
+              ->constrained('users')
+              ->nullOnDelete();
+
+        $table->bigInteger('amount');
+
+        $table->string('type');
+
+        $table->string('status')->default('success');
+
+        $table->string('reference')->unique();
+
+        $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
