52a216d513b06d0ee69b8790ccf3e685af49fbf1
Koumolou <learningsin2024@gmail.com>
2026-03-08 14:34:55 +0000

    Fix login route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'BankApp API is running.']);
 });
 
-
+Route::get('/login', function () {
+    return response()->json(['message' => 'Please use the frontend to login.']);
+})->name('login');
