f01235f1fa013ee1db7fcc2526e3d57ad27a30a7
Koumolou <learningsin2024@gmail.com>
2026-03-08 14:13:05 +0000

    Add test file

---
+++ public/test.php
@@ -0,0 +1,7 @@
+<?php
+echo "PHP is working";
+echo "<br>APP_KEY: " . getenv('APP_KEY');
+echo "<br>DB_HOST: " . getenv('DB_HOST');
+echo "<br>DB_PORT: " . getenv('DB_PORT');
+echo "<br>DB_DATABASE: " . getenv('DB_DATABASE');
+echo "<br>DB_USERNAME: " . getenv('DB_USERNAME');
